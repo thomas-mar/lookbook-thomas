a98ab169e111441929482217ff5bcebda64e4316
Thomas <30400633+thomas-mar@users.noreply.github.com>
2026-05-24 23:34:40 +1000

    Build lookbook section

---
+++ assets/lookbook.js
@@ -0,0 +1,149 @@
+
+class Lookbook extends HTMLElement {
+
+  connectedCallback() {
+    this.productHandles = JSON.parse(this.getAttribute('product-handles') || '[]');
+    this.getProducts();
+  }
+
+
+  async getProducts() {
+    try {
+        const query = `
+        query LookbookProducts(
+          $country: CountryCode,
+          $language: LanguageCode
+        )
+        @inContext(country: $country, language: $language) {
+
+          ${this.productHandles
+            .map(
+              (handle, index) => `
+                product${index}: product(handle: "${handle}") {
+                  id
+                  title
+                  handle
+      
+                  featuredImage {
+                    url
+                    altText
+                  }
+      
+                  priceRange {
+                    minVariantPrice {
+                      amount
+                      currencyCode
+                    }
+                  }
+      
+                  compareAtPriceRange {
+                    minVariantPrice {
+                      amount
+                      currencyCode
+                    }
+                  }
+                }
+              `,
+            )
+            .join('\n')}
+        }
+      `;
+
+      const response = await fetch('/api/2025-01/graphql.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Shopify-Storefront-Access-Token': storeFrontAccessToken,
+        },
+        body: JSON.stringify(
+            { query, 
+              variables: {
+                country: this.dataset.country,
+                language: this.dataset.language,
+              },
+            }
+        ),
+      });
+
+      const result = await response.json();
+      const products = result.data
+
+      this.renderProducts(products);
+    } catch (error) {
+      console.error('Lookbook fetch failed', error);
+    }
+  }
+
+
+  async renderProducts(products){
+    const container = this.querySelector('.lookbook-products');
+    if (!container) return;
+
+    const productsArray = Object.values(products).filter(Boolean);
+
+    container.innerHTML = productsArray.map((product) => {
+    const url = `/products/${product.handle}`;
+    const image = product.featuredImage;
+
+    const price = product.priceRange?.minVariantPrice;
+    const compareAtPrice = product.compareAtPriceRange?.minVariantPrice;
+
+      const formattedPrice = price
+      ? new Intl.NumberFormat(undefined, {
+          style: 'currency',
+          currency: price.currencyCode,
+          currencyDisplay: 'narrowSymbol',
+        }).format(parseFloat(price.amount))
+      : '';
+    
+    const formattedCompareAtPrice = compareAtPrice
+      ? new Intl.NumberFormat(undefined, {
+          style: 'currency',
+          currency: compareAtPrice.currencyCode,
+          currencyDisplay: 'narrowSymbol',
+        }).format(parseFloat(compareAtPrice.amount))
+      : '';
+
+    const isOnSale = compareAtPrice && parseFloat(compareAtPrice.amount) > parseFloat(price.amount);
+
+    return `
+      <a href="${url}" class="lookbook-product-card">
+        ${
+          image
+            ? `
+              <img
+                src="${image.url}"
+                alt="${image.altText ?? product.title}"
+                loading="lazy"
+              >
+            `
+            : ''
+        }
+
+        <p class="lookbook-product-title">${product.title}</p>
+
+        <div class="lookbook-product-price h6">
+          ${
+            isOnSale
+              ? `
+                <span class="lookbook-product-price-sale">
+                  ${formattedPrice}
+                </span>
+
+                <s class="lookbook-product-price-compare">
+                  ${formattedCompareAtPrice}
+                </s>
+              `
+              : formattedPrice
+          }
+        </div>
+      </a>
+    `;
+  })
+  .join('');
+    }
+}
+
+if (!customElements.get('look-book')) {
+  customElements.define('look-book', Lookbook);
+}
---
+++ assumptions.md
@@ -0,0 +1,49 @@
+#LOOKBOOK SECTION
+
+Convert Digital Technical Assessment to build a native Shopify Lookbook feature. Built on the Horizon theme, using metaobjects for content modelling and using the Storefront API for runtime product data retrival. Support for the store Australia (AUD) and Japan (JPY) markets.
+
+
+## ASSUMPTIONS/DESCIONS
+
+- Assumed theme was built using the default Horizon Theme
+- Used Sidekick to generate 10 fashion products for the AU and JP markets.
+- If a product appears in multiple lookbooks we return the first available lookbook from reverse lookup.
+- Non PDP Lookbook requires a lookbook to be assigned. If lookbook is selected nothing shows.
+- Used light styling to make section presentable.
+- For simplicity and demonstrating functionality - CSS was added to the liquid template and JS (lookbook.js) to the /assets folder.
+- Storefront API Token was created via Headless App instead of via Custom App in Dev Dashboard.
+
+
+
+
+## ARCHITECTURE
+
+Assumed three fields for simplicity and demonstration of data retrival from metaobject and graphQL. 
+
+- title (Single Line Text)
+- description (Rich Text)
+- product_handles (Single Line Text - List)
+
+Two section templates one limited for use template Product and other disabled on products.
+
+
+###PDP
+By default, lookbook section uses product's handle to perform a reverse lookup for a lookbook metaobject containing current product exists. If it exists it performs a storefront API request via graphQL to retrieve all products from the lookbook entry.
+
+Option to manually override section with specific lookbook. 
+
+
+### Non PDP
+
+If no lookbook is selected nothing shows.
+
+
+
+
+
+## DEMONSTRATION
+
+Non PDP Lookbook section set up on Home page template to reference "Spring's Suprise Newest Look" etaobject entry.
+
+
+
